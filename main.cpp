61297def2753aa48bd2dd22b8752f5d8720acf67
yl-ang <yongliangang@gmail.com>
2024-12-13 16:48:35 +0800

    Update cmake to use nlohmann_json and sfml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Candle Stick Project!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Candlestick Chart");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
     return 0;
 }
