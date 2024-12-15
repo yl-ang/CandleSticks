deb7c8c15c05137ee8641630c8b8c6f1016a99dd
yl-ang <yongliangang@gmail.com>
2024-12-15 16:32:48 +0800

    Ensure tooltip is displayed within chart boundary

--- src/Tooltip.cpp
+++ src/Tooltip.cpp
@@ -24,9 +24,21 @@ void Tooltip::update(const sf::Vector2f &mousePosition, const Candlestick &candl
     tooltipText.setString(details.str());
 
     tooltipBackground.setSize(sf::Vector2f(tooltipText.getGlobalBounds().width + 10.0f, tooltipText.getGlobalBounds().height + 10.0f));
-    tooltipBackground.setPosition(mousePosition.x + 10.0f, mousePosition.y + 10.0f);
-    tooltipText.setPosition(mousePosition.x + 15.0f, mousePosition.y + 15.0f);
+
+    sf::Vector2f tooltipPos(mousePosition.x + 10.0f, mousePosition.y + 10.0f);
+
+    if (tooltipPos.x + tooltipBackground.getSize().x > 800) {
+        tooltipPos.x = mousePosition.x - tooltipBackground.getSize().x - 10.0f;
+    }
+
+    if (tooltipPos.y + tooltipBackground.getSize().y > 650) {
+        tooltipPos.y = mousePosition.y - tooltipBackground.getSize().y - 10.0f;
+    }
+
+    tooltipBackground.setPosition(tooltipPos);
+    tooltipText.setPosition(tooltipPos.x + 5.0f, tooltipPos.y + 5.0f);
 }
+
 
 void Tooltip::draw(sf::RenderWindow &window) {
     window.draw(tooltipBackground);
